868411e44d1407ad2a800f2162c4632bd65014fc
lavishtembhare <lavishtembhare93@gmail.com>
2025-11-15 15:35:07 +0530

    Add Inheritance-Mapping module

---
+++ Inheritance-Mapping/Table-Per-Hierarchy/src/main/java/org/lavish/Regular_Employee.java
@@ -0,0 +1,22 @@
+package org.lavish;
+
+public class Regular_Employee extends Employee{
+    private float salary;
+    private int bonus;
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
