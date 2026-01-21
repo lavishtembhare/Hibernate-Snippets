d7411855412d65eb597811ae56f71bbfe50210f0
lavishtembhare <lavishtembhare93@gmail.com>
2026-01-22 01:26:59 +0530

    Update

--- Hibernate-Snippet/src/main/java/org/example/Main.java
+++ Hibernate-Snippet/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import jdk.jshell.Snippet;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -9,15 +10,16 @@ public class Main {
     public static void main(String[] args) {
 
         Student s = new Student();
-        s.setsName("John");
-        s.setRollno(105);
-        s.setsAge(27);
+        s.setsName("Johnny");
+        s.setRollno(106);
+        s.setsAge(23);
 
-        Configuration cfg = new Configuration();
-        cfg.configure(); // loads hibernate.cfg.xml
-        cfg.addAnnotatedClass(Student.class);
+//        Configuration cfg = new Configuration();
+//        cfg.configure(); // loads hibernate.cfg.xml
+//        cfg.addAnnotatedClass(Student.class);
 
-        SessionFactory sf = cfg.buildSessionFactory();
+//        SessionFactory sf = cfg.buildSessionFactory();
+        SessionFactory sf = new Configuration().addAnnotatedClass(org.example.Student.class).configure().buildSessionFactory();
         Session session = sf.openSession();
 
         Transaction tx = session.beginTransaction();
